883eb657eaffdc9044180f60d88430fabb2680e0
YOUNGJUNE <cerealmaster@naver.com>
2023-12-13 13:36:38 +0900

    README.md modified

--- src/time.cpp
+++ src/time.cpp
@@ -7,12 +7,12 @@
 /*
 ** TimeCalc::get_crack_time(length of entire password, modifier, required_string)
 ** OUTPUT -> returns calculated time(seconds).
-** Modifier^(entire password - required string) / 10^9 * weight*5
+** Time taken = Modifier^(entire lengths) / 10^12 * weight*5
 */
 uint64_t TimeCalc::get_crack_time(uint8_t length, uint8_t modifier, std::string required_string){
     double weight = required_string.length() > 0 ? get_weight(required_string) : 0.1;
     double log_numerator = length * log(modifier);
-    double log_denominator = log(pow(10, 9)) + log(5 * weight);
+    double log_denominator = log(pow(10, 12)) + log(5 * weight);
     double result = exp(log_numerator - log_denominator);
     return static_cast<uint64_t>(result);
 }
@@ -20,7 +20,7 @@ uint64_t TimeCalc::get_crack_time(uint8_t length, uint8_t modifier, std::string 
 /*
 ** TimeCalc::get_weight(required string)
 ** OUTPUT -> max percentage of how much given string look alike to the password lists
-** 
+** Weight = how many characters look alike in the password/length of the line
 */
 double TimeCalc::get_weight(std::string required_password){
     std::string filepath = "password.txt";
@@ -51,10 +51,14 @@ double TimeCalc::get_weight(std::string required_password){
 ** prints seconds to days/months/years/centuries
 */
 void TimeCalc::print_time(uint64_t time){
-    double seconds_per_day = 86400, seconds_per_month = 86400*30, seconds_per_year = 86400*30*12, seconds_per_century = 3155673600;
-    if (time< seconds_per_day){
-        std::cout<<std::fixed << std::setprecision(2)<<"It takes about "<<time<<" seconds to crack this password. \n";
-    }else if (time < seconds_per_month) {
+    double seconds_per_minutes = 60, seconds_per_hours = 3600, seconds_per_day = 86400, seconds_per_month = 86400*30, seconds_per_year = 86400*30*12, seconds_per_century = 3155673600;
+    if(time<seconds_per_minutes){
+        std::cout<<std::fixed << std::setprecision(2)<<"It takes about "<<time<<" seconds to crack this password. \n";        
+    } else if(time<seconds_per_hours){
+        std::cout<<std::fixed << std::setprecision(2)<<"It takes about "<<time/seconds_per_minutes<<" minutes to crack this password. \n";        
+    } else if (time< seconds_per_day){
+        std::cout<<std::fixed << std::setprecision(2)<<"It takes about "<<time/seconds_per_hours<<" hours to crack this password. \n";
+    } else if (time < seconds_per_month) {
         std::cout<<std::fixed << std::setprecision(2)<<"It takes about "<<time/seconds_per_day<<" days to crack this password. \n";
     } else if (time < seconds_per_year) {
         std::cout<<std::fixed << std::setprecision(2)<<"It takes about "<<time/seconds_per_month<<" months to crack this password. \n";
